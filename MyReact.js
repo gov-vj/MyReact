478b1b1fa0db0eb56961de82109fba230c0b3d89
Govind Vijay <gv.vj1993@gmail.com>
2023-11-25 00:01:04 +0530

    One way to fix is to change state to the getter function.
    But in react you don't call the getter function.

--- MyReact.js
+++ MyReact.js
@@ -1,12 +1,13 @@
 "use strict";
 const useState = (initialValue) => {
-    let state = initialValue;
+    let _val = initialValue;
+    const state = () => _val;
     const setState = (newValue) => {
-        state = newValue;
+        _val = newValue;
     };
     return [state, setState];
 };
 const [count, setCount] = useState(0);
-console.log(count); // 0
+console.log(count()); // 0
 setCount(1);
-console.log(count); // 0 --> doesn't get updated
+console.log(count()); // 1
--- MyReact.ts
+++ MyReact.ts
@@ -1,12 +1,13 @@
-const useState = <T>(initialValue: T): [T, (newValue: T) => void] => {
-    let state = initialValue;
+const useState = <T>(initialValue: T): [() => T, (newValue: T) => void] => {
+    let _val = initialValue;
+    const state = () => _val;
     const setState = (newValue: T) => {
-        state = newValue;
+        _val = newValue;
     };
     return [state, setState];
 }
 
 const [count, setCount] = useState(0);
-console.log(count);  // 0
+console.log(count());  // 0
 setCount(1);
-console.log(count);  // 0 --> doesn't get updated
+console.log(count());  // 1
